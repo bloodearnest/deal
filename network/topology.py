52bfa2a0a86c5fee3eb1ed6fd215c4072b6cc667
Simon Davy <wavy@comp.leeds.ac.uk>
2008-09-05 11:24:24 +0100

    Added preferential attachment

--- network/topology.py
+++ network/topology.py
@@ -3,7 +3,7 @@ import math, random
 class FullyConnectedNode(StandardError):
     pass
 
-def generative_link(G, node, p_local):
+def generative_link(G, node, p_local, p_pref):
     # discount self and neighbors
     all_nodes = G.nodes()
     all_nodes.remove(node)
@@ -11,7 +11,7 @@ def generative_link(G, node, p_local):
         all_nodes.remove(n)
 
 
-    if random.random() < p_local: # local
+    if p_local and random.random() < p_local: # local
         nds = [n for n in all_nodes if n.region == node.region]
     else: # global
         nds = [n for n in all_nodes if n.region != node.region]
@@ -24,23 +24,38 @@ def generative_link(G, node, p_local):
         print len([n for n in all_nodes if n.region == node.region])
         print len([n for n in all_nodes if n.region != node.region])
         raise FullyConnectedNode()
+    
+    if p_pref and random.random() < p_pref:
+        other = roulette_select(nds)
+        if other:
+            return other
+        else:
+            return random.choice(nds)
+    else:
+        return random.choice(nds)
 
-    return random.choice(nds)
+def roulette_select(nds):
+    choices = []
+    for n in nds:
+        choices += [n for i in range(n.degree)]
+    if choices:
+        return random.choice(choices)
+    else:
+        return None
 
-
-def generative_topology(G, p_local = 0.7):
+def generative_topology(G, p_local = 0.0, p_pref = 0.0):
     nodes = G.nodes()
     size = len(nodes)
     max_edges = size * G.mean_degree / 2 # 1 ling = +2 degrees!
     
     # give everyone a link
     for node in nodes:
-            G.make_link(node, generative_link(G, node, p_local))
+        G.make_link(node, generative_link(G, node, p_local, p_pref))
         
     edges = size
     while edges < max_edges:
         node = random.choice(nodes)
-        G.make_link(node, generative_link(G, node, p_local))
+        G.make_link(node, generative_link(G, node, p_local, p_pref))
         edges += 1
 
 
